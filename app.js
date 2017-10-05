665baedf75f8740f80cda958f246ee3973e4de59
Pavel Prorovsky <prorovsky@gmail.com>
2017-10-05 21:58:44 +0300

    add events for blocks

---
+++ app.js
@@ -0,0 +1,65 @@
+const greenBlock = document.querySelector("#green"),
+      redBlock = document.querySelector("#red"),
+      orangeBlock = document.querySelector("#orange"),
+      blueBlock = document.querySelector("#blue");
+
+greenBlock.originColor = "darkgreen";
+greenBlock.newColor = "green";
+redBlock.originColor = "darkred";
+redBlock.newColor = "red";
+blueBlock.originColor = "darkblue";
+blueBlock.newColor = "blue";
+orangeBlock.originColor = "darkorange";
+orangeBlock.newColor = "orange";
+
+greenBlock.addEventListener("click", changePropertyOfBlock);
+redBlock.addEventListener("click", changePropertyOfBlock);
+blueBlock.addEventListener("click", changePropertyOfBlock);
+orangeBlock.addEventListener("click", changePropertyOfBlock);
+
+function changePropertyOfBlock(event) {
+    changeColorOfBlock(event.target);
+    changeSizeOfBlock(event.target);
+    disableEvents();
+    resetBlockBack(event.target);
+}
+
+function resetBlockBack(colorBlock) {
+    setTimeout(changeColorBlockToOrigin.bind(null, colorBlock), 500);
+}
+
+function changeColorBlockToOrigin(colorBlock) {
+    changeColorToOrigin(colorBlock);
+    changeSizeToOrigin(colorBlock);
+    enableEvents();
+}
+
+function changeColorOfBlock(colorBlock) {
+    colorBlock.style.backgroundColor = colorBlock.newColor;
+}
+
+function changeSizeOfBlock(colorBlock) {
+    colorBlock.style.transform = ("scale(1.09, 1.09)");
+}
+
+function changeColorToOrigin(colorBlock) {
+    colorBlock.style.backgroundColor = colorBlock.originColor;   
+}
+
+function changeSizeToOrigin(colorBlock) {
+    colorBlock.style.transform = ("scale(1, 1)");
+}
+
+function disableEvents() {
+    redBlock.style.pointerEvents = 'none';
+    blueBlock.style.pointerEvents = 'none';
+    orangeBlock.style.pointerEvents = 'none';
+    greenBlock.style.pointerEvents = 'none';
+}
+
+function enableEvents() {
+    redBlock.style.pointerEvents = 'auto';
+    blueBlock.style.pointerEvents = 'auto';
+    orangeBlock.style.pointerEvents = 'auto';
+    greenBlock.style.pointerEvents = 'auto';
+}
